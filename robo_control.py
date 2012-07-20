bf043a0182b00624d107f01d4b8f27e7936f9030
Filipi Bardusco <filipi@bardusco.com>
2012-07-19 21:00:54 -0300

    melhorando a interface de controle

--- robo_control.py
+++ robo_control.py
@@ -4,6 +4,9 @@ import nxt.locator
 from nxt.sensor import *
 import time
 from nxt.motor import *
+import sys
+import tty
+tty.setcbreak(sys.stdin)
 
 metodo = nxt.locator.Method(bluetooth=True)
 print"procurando robo..."
@@ -11,7 +14,6 @@ b = nxt.locator.find_one_brick(method=metodo)
 print"robo localizado"
 motor_esquerdo = Motor(b, PORT_B)
 motor_direito = Motor(b, PORT_C)
-
 
 # FORWARD
 def f():
@@ -39,12 +41,24 @@ def l():
 	motor_esquerdo.run (100)
 
 while True:
-	comando = raw_input("Digite o comando para controlar o robo: ")
-	print "you entered ", comando
-	try:
-		eval(comando+"()")
-	except:
-		print "comando esta errado"
+    tecla=ord(sys.stdin.read(1))
+    if tecla==65:
+        f()
+        print "frente"
+    elif tecla==66:    
+        b() 
+        print "tras"
+    elif tecla==67:    
+        r()
+        print "direita"
+    elif tecla==68:    
+        l()
+        print "esquerda"
+    elif tecla==32:    
+        s()
+        print "para"
+    else:
+        print tecla
 
 
 #	if Touch(b,PORT_1).get_sample() == True :

c5927fb128dbb4a1b18720ca00bc096a86179876
Danilo Bardusco <bardusco@corp.globo.com>
2012-08-11 18:03:54 -0300

    adicionado comandos para atirar e tocar musicas

--- robo_control.py
+++ robo_control.py
@@ -4,16 +4,24 @@ import nxt.locator
 from nxt.sensor import *
 import time
 from nxt.motor import *
+from nxt.bluesock import *
 import sys
 import tty
 import termios
 
 metodo = nxt.locator.Method(bluetooth=True)
 print"procurando robo..."
-b = nxt.locator.find_one_brick(host="00:16:53:1A:68:D8", method=metodo)
+#b = nxt.locator.find_one_brick(host="00:16:53:1A:68:D8", method=metodo)
+b = nxt.bluesock.BlueSock('00:16:53:1A:68:D8').connect()
+time.sleep(1)
+b.play_tone_and_wait(1000,500)
+b.play_tone_and_wait(2,500)
+b.play_tone_and_wait(1000,500)
 print"robo localizado"
+
 motor_esquerdo = Motor(b, PORT_C)
 motor_direito = Motor(b, PORT_B)
+motor_meio = Motor(b, PORT_A)
 
 # FORWARD
 def frente():
@@ -44,8 +52,23 @@ def quit():
 	old[3] = old[3] | termios.ECHO
 	termios.tcsetattr(fd, termios.TCSADRAIN, old)
 	sys.exit()
+
 def distancia():
 	print 'Ultrasonic:', Ultrasonic(b, PORT_4).get_sample()
+
+def cor():
+	cores=['nao definida', 'black', 'blue', 'green', 'yellow', 'red', 'white']
+        print 'Cor:', cores[Color20(b, PORT_3).get_sample()]
+
+def atirar():
+	motor_meio.turn (110,360)
+	
+# Nao temos esse sensor ainda
+def som():
+	print 'Sound:', Sound(b, PORT_2).get_sample()
+	
+def musica():
+	b.play_sound_file(0,"C-r2-d2.rso")
 
 # Grava as configuracoes de terminal para resetar antes de sair
 fd = sys.stdin.fileno()
@@ -74,6 +97,14 @@ while True:
         print "quit"
     elif tecla==100:
          distancia()
+    elif tecla==115:
+         som()
+    elif tecla==99:
+         cor()
+    elif tecla==97:
+         atirar()
+    elif tecla==109:
+         musica()
     else:
         print tecla
 
